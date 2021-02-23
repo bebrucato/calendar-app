23996c40a674b15d2556d74c01f0074b5598e5dd
B Brucato <bebrucato@gmail.com>
2021-02-22 18:43:24 -0800

    fixed time and save button

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-//Checking for the time and assigning new classes that match
-//the CSS styling sheet
-    function eachHour() {
-      // get current number of hours
+$(document).ready(function() {
+
+//Current day/date under header
+    $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+
+//Event listener for a click on the "save button,
+//which allows the information to be saved to the
+//text area/rows
+    $(".saveBtn").on("click", function() {
+      var time = $(this).parent().attr("id");
+      var value = $(this).siblings(".description").val();
+      
+      localStorage.setItem(time, value);
+    });
+  
+//Checks for current time using the "time-block" classes
+    function timeCheck() {
       var currentT = moment().hours();
   
-      // loop over time blocks
       $(".time-block").each(function() {
         var blockT = parseInt($(this).attr("id").split("-")[1]);
   
-        // check if we've moved past this time
         if (blockT < currentT) {
           $(this).addClass("past");
         } 
+
         else if (blockT === currentT) {
           $(this).removeClass("past");
           $(this).addClass("present");
         } 
+
         else {
           $(this).removeClass("past");
           $(this).removeClass("present");
@@ -23,18 +36,24 @@
         }
       });
     }
+
+  //Function callback
+    timeCheck();
   
-    eachHour();
+    //The interval checks for time change/hour change
+    var interval = setInterval(timeCheck, 10000);
   
-   $(".saveBtn").on("click", function() {
-          var value = $(this).siblings(".description").val();
-          var time = $(this).parent().attr("id");
-      
-          localStorage.setItem(time, value);
-    });
-
-    var interval = setInterval(hourUpdater, 15000);
-
-    $("#currentDay").text(moment().format("dddd, MMMM yy"));
-
-      
+    // Saves the data in "text area" to local storage.
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    
+  });
+  
