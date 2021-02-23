5ee5be03cafb1bb9bc6deb698a6c04a50bcd7cb1
B Brucato <bebrucato@gmail.com>
2021-02-22 16:43:31 -0800

    function for the hours

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 //Current day/date
 var currentDay= $("#currentDay")
 currentDay.text(moment().format("dddd, MMMM Do, YYYY"))
+
+//Checking for the time and assigning new classes that match
+//the CSS styling sheet
+function eachHour() {
+    
+    var currentHour = moment().hours();
+
+    $(".time-block").each(function() {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } 
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } 
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  eachHour();
