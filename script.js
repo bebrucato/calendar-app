c263fe22129e80e3903ba62b333af582d72e3253
B Brucato <bebrucato@gmail.com>
2021-02-22 17:10:42 -0800

    fixed the function to save colors

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
-//Current day/date
-var currentDay= $("#currentDay")
-currentDay.text(moment().format("dddd, MMMM Do, YYYY"))
+
 
 //Checking for the time and assigning new classes that match
 //the CSS styling sheet
-function eachHour() {
+
+  
+    function eachHour() {
+      // get current number of hours
+      var currentT = moment().hours();
+  
+      // loop over time blocks
+      $(".time-block").each(function() {
+        var blockT = parseInt($(this).attr("id").split("-")[1]);
+  
+        // check if we've moved past this time
+        if (blockT < currentT) {
+          $(this).addClass("past");
+        } 
+        else if (blockT === currentT) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } 
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+      });
+    }
+  
+    eachHour();
+  
+   
     
-    var currentHour = moment().hours();
-
-    $(".time-block").each(function() {
-      var blockHour = parseInt($(this).attr("id").split("-")[1]);
-
-      if (blockHour < currentHour) {
-        $(this).addClass("past");
-      } 
-      else if (blockHour === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-      } 
-      else {
-        $(this).removeClass("past");
-        $(this).removeClass("present");
-        $(this).addClass("future");
-      }
-    });
-  }
-
-  eachHour();
